30d9db9a8bf6b675bb29bce2ea773b5011c9c1fd
ide03088 <ide03088@naver.com>
2019-07-05 15:37:34 +0900

    버전 관리 시작

---
+++ B3_ReadsString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class B3_ReadsString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자열 입력 :");
+		String str1 = sc.nextLine();
+		
+		System.out.print("문자열 입력 :");
+		String str2 = sc.nextLine();
+		
+		System.out.printf("입력된 문자열 1:%s \n",str1);
+		System.out.printf("입력된 문자열 2:%s \n",str2);
+		
+//		System.out.println("입력된 문자열 3: "+str1+str2);
+	
+
+	}
+
+}
