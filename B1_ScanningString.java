a89bf5e27d79418437657a98175aeba3c492bddf
ide03088 <ide03088@naver.com>
2019-07-05 15:48:36 +0900

    한글 데이터 스트림으로 읽기

--- B1_ScanningString.java
+++ B1_ScanningString.java
@@ -3,13 +3,13 @@
 public class B1_ScanningString {
 
    public static void main(String[] args) {
-      String source = "1 3 5";
+      String source = "홍 길 동";
 	  // 스트링 데이터 읽기
       Scanner sc = new Scanner(source);
       
-      int num1 = sc.nextInt();
-      int num2 = sc.nextInt();
-      int num3 = sc.nextInt();
+      int num1 = sc.next();
+      int num2 = sc.next();
+      int num3 = sc.next();
       
       int sum = num1 + num2 + num3 ;
       System.out.printf("%d + %d + %d = %d \n", num1, num2, num3, sum);
